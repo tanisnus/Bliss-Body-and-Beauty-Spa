918d508ab84fe3d4652966a5da0d7f74ab35bb1b
tanisnus <taniss246@gmail.com>
2025-07-21 16:15:19 -0700

    Service Page (Image on left & description on right)
    Service Page (Image on left & description on right)

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import scrub_image from '../images/body-scrub.jpg'
 import './Service.css'
+import PricingTable from 'react-pricing-table';
+
 
 export default function Service() {
     return (
@@ -11,8 +13,18 @@ export default function Service() {
                 <div className='scrub-detail-title-div'> 
                     <h1>Body Scrub</h1>
                 </div>
-                <div className='scrub-detail-description-div'>
 
+                <div className='scrub-detail-bulletpoint-div'>
+                    <ul className='scrub-detail-bulletpoint-list'>
+                        <li> Unscented Salt Scrub </li>
+                        <li> Green Tea & Peppermint Salt Scrub</li>
+                        <li> Unscented Brown sugar scrub </li>
+                        <li> French Vanilla Latte Organic Brown Sugar Scrub </li>
+                        <li> Vanilla Orange Brown Sugar Scrub </li>
+                    </ul>
+                </div>
+
+                <div className='scrub-detail-pricing-div'>
                 </div>
 
             </div>
