2e49a2bef12ed34c23387a64563dddb4cd38e8e6
tanisnus <taniss246@gmail.com>
2025-07-21 16:52:55 -0700

    fixed import syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import Header from './components/header'
+import Header from './components/Header'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // import pages
