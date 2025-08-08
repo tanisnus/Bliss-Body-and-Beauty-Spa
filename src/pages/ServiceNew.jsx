c309791ae7eeb65d04d0792fcc7ddf93fc52d98e
tanisnus <taniss246@gmail.com>
2025-08-07 20:38:25 -0700

    Working on service section

--- src/pages/ServiceNew.jsx
+++ src/pages/ServiceNew.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import './ServiceNew.css'
 
+
+import scrub_image from '../images/body-scrub.jpg'
+import body_wraps_image from '../images/body-wrap.jpg'
+import massage_image from '../images/massage.jpg'
+
 import spaLogo from '../images/Spa_Logo.png'
 
 export default function Service() {
@@ -15,8 +20,6 @@ export default function Service() {
             {/* Intro Section */}
             <div className='intro-div'>
 
-
-
                 {/* Image */}
                 <div className='image-div'>
                     <img className='image' src={spaLogo} alt="Brand Image"/>
@@ -24,18 +27,13 @@ export default function Service() {
 
                 {/* Text */}
                 <div className='text-div'>
-
                     <h1 className='text-header'>
                         Welcome!
                     </h1>
-
                     <p className='text-body'>
                         At our wellness studio, we offer high-quality body care treatments like natural scrubs, hydrating wraps, and post-surgery therapies such as lymphatic drainage and red light therapy—to help you feel refreshed, relaxed, and restored.                
                     </p>
-
                 </div>
-
-
 
 
             </div>
@@ -43,8 +41,57 @@ export default function Service() {
 
 
             {/* All Services Section */}
-            <div className='all-services-div'>
-                All Services Section
+            <div className='services-grid'>
+
+                <div className='services-row'>
+                row 1
+                    <div className='service-card'>
+                        <img src={scrub_image}></img>
+                        <h3> Body Scrub</h3>
+                        <p> Description</p>
+                    </div>
+
+
+                    <div className='service-card'>
+                        <img></img>
+                        <h3> Service 2</h3>
+                        <p> Description</p>
+                    </div>
+
+
+                    <div className='service-card'>
+                        <img></img> 
+                        <h3> Service 3</h3>
+                        <p> Description</p>
+
+                    </div>   
+                </div>
+
+                <div className='services-row'>
+                    row 2
+                    <div className='service-card'>
+                        <img ></img>
+                        <h3> Service 4</h3>
+                        <p> Description</p>
+                    </div>
+
+
+                    <div className='service-card'>
+                        <img></img>
+                        <h3> Service 5</h3>
+                        <p> Description</p>
+                    </div>
+
+
+                    <div className='service-card'>
+                        <img></img> 
+                        <h3> Service 6</h3>
+                        <p> Description</p>
+
+                    </div>   
+                </div>
+
+
             </div>
 
         </div>
