9b153359d690344ab91f56a91e4885a9a61ed152
tanisnus <taniss246@gmail.com>
2025-08-14 00:33:00 -0700

    Basic Page Set Up

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // import pages
 import ServiceNew from './pages/ServiceNew';
+import Service3 from './pages/Service3';
 import Contact from './pages/Contact';
 
 function App() {
@@ -19,8 +20,8 @@ function App() {
 
         <section className='main-content-section'>
           <Routes>
-            <Route path='/' element={<ServiceNew />} />
-            <Route path='/service' element={<ServiceNew />} />     
+            <Route path='/' element={<Service3 />} />
+            <Route path='/service' element={<Service3 />} />     
             <Route path='/contact' element={<Contact />} />
           </Routes>
         </section>
---
+++ src/pages/Service3.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './Service3.css'
+
+export default function Service3() {
+    return (
+        <div className='default-page-div'>
+            
+
+             {/* Images Section */}
+            <div className='images-div'>
+
+            </div>
+
+            {/* Services Section */}
+            <div className='services-div'>
+
+            </div>
+
+            {/* Quote Section */}
+            <div className='quote-div'>
+
+            </div>
+
+
+            {/* Description Section */}
+            <div className='description-div'>
+
+            </div>
+
+            {/* More Image Section */}
+            <div className='more-images-div'>
+
+            </div>
+
+            {/* Contact Section */}
+            <div className='contact-div'>
+
+            </div>
+
+
+            
+        </div>
+    )
+}
